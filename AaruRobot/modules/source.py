81f3ff4e94e8e56354254a4009448f0a50e5d02c
BANNA_XD <105870422+BANNA-XD143@users.noreply.github.com>
2022-06-28 21:35:41 +0530

    Update source.py

--- AaruRobot/modules/source.py
+++ AaruRobot/modules/source.py
@@ -4,16 +4,16 @@ from pyrogram import __version__ as z
 from telethon import __version__ as s
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram import filters
-from AyraRobot import pbot as client
+from AaruRobot import pbot as client
 
 
-Ayra = "https://telegra.ph/file/395d6af16f462c011b19b.jpg"
+Aaru = "https://telegra.ph/file/6ccab5bc360325388e2c4.jpg"
 
 @client.on_message(filters.command(["repo", "source"]))
 async def repo(client, message):
     await message.reply_photo(
         photo=Ayra,
-        caption=f"""**ʜᴇʏ​ {message.from_user.mention()},\n\nɪ ᴀᴍ [ᴀʏʀᴀ ✘ ʀᴏʙᴏᴛ-🇮🇳](t.me/Miss_Ayra_bot)**
+        caption=f"""**ʜᴇʏ​ {message.from_user.mention()},\n\nɪ ᴀᴍ [ᴀᴀʀᴜ ✘ ʀᴏʙᴏᴛ-🇮🇳](t.me/MISS_AARU_X_ROBOT)**
 
 **» ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ​ :** [❛-𝐌𝐑'𝐁𝐀𝐍𝐍𝐀 🚬 𝐊𝐈𝐍𝐆-𝐱𝐃 ° ](tg://user?id=5489344295)
 **» ᴩʏᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{y()}`
@@ -21,7 +21,7 @@ async def repo(client, message):
 **» ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{s}` 
 **» ᴘʏʀᴏɢʀᴀᴍ ᴠᴇʀsɪᴏɴ :** `{z}`
 
-**ᴀʏʀᴀ ✘ ʀᴏʙᴏᴛ sᴏᴜʀᴄᴇ ɪs ɴᴏᴡ ᴩᴜʙʟɪᴄ ᴀɴᴅ ɴᴏᴡ ʏᴏᴜ ᴄᴀɴ ᴍᴀᴋᴇ ʏᴏᴜʀ ᴏᴡɴ ʙᴏᴛ.**
+**ᴀᴀʀᴜ ✘ ʀᴏʙᴏᴛ sᴏᴜʀᴄᴇ ɪs ɴᴏᴡ ᴩᴜʙʟɪᴄ ᴀɴᴅ ɴᴏᴡ ʏᴏᴜ ᴄᴀɴ ᴍᴀᴋᴇ ʏᴏᴜʀ ᴏᴡɴ ʙᴏᴛ.**
 """,
         reply_markup=InlineKeyboardMarkup(
             [
@@ -29,7 +29,7 @@ async def repo(client, message):
                     InlineKeyboardButton(
                         "• ᴏᴡɴᴇʀ •", url="tg://user?id=5489344295"), 
                     InlineKeyboardButton(
-                        "• sᴏᴜʀᴄᴇ •", url="https://github.com/MISS-AYRA/AyraRobot")
+                        "• sᴏᴜʀᴄᴇ •", url="https://github.com/BANNA-XD143/AaruRobot")
                 ]
             ]
         )
