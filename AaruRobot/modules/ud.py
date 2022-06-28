bc3eaab7ce530fff846f5438259aefcfc2a05af1
BANNA_XD <105870422+BANNA-XD143@users.noreply.github.com>
2022-06-28 21:14:40 +0530

    Update ud.py

--- AaruRobot/modules/ud.py
+++ AaruRobot/modules/ud.py
@@ -1,6 +1,6 @@
 import requests
-from AyraRobot import dispatcher
-from AyraRobot.modules.disable import DisableAbleCommandHandler
+from AaruRobot import dispatcher
+from AaruRobot.modules.disable import DisableAbleCommandHandler
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, run_async
 
