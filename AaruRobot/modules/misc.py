76d3925e682b97e110c61159ea6e283c935c34af
BANNA_XD <105870422+BANNA-XD143@users.noreply.github.com>
2022-06-28 22:00:26 +0530

    Update misc.py

--- AaruRobot/modules/misc.py
+++ AaruRobot/modules/misc.py
@@ -1,6 +1,6 @@
-from AyraRobot.modules.helper_funcs.chat_status import user_admin
-from AyraRobot.modules.disable import DisableAbleCommandHandler
-from AyraRobot import dispatcher
+from AaruRobot.modules.helper_funcs.chat_status import user_admin
+from AaruRobot.modules.disable import DisableAbleCommandHandler
+from AaruRobot import dispatcher
 
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from telegram import ParseMode, Update
