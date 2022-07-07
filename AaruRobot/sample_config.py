1540f21950194c7318499d7d8483d39d4e241c5b
BANNA_XD <105870422+BANNA-XD143@users.noreply.github.com>
2022-07-07 07:13:56 +0530

    Update sample_config.py

--- AaruRobot/sample_config.py
+++ AaruRobot/sample_config.py
@@ -4,7 +4,7 @@ import os
 
 
 def get_user_list(config, key):
-    with open("{}/FallenRobot/{}".format(os.getcwd(), config), "r") as json_file:
+    with open("{}/AaruRobot/{}".format(os.getcwd(), config), "r") as json_file:
         return json.load(json_file)[key]
 
 
@@ -17,14 +17,14 @@ class Config(object):
     API_ID = 123456  # integer value, dont use ""
     API_HASH = "awoo"
     TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 5230222509  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "COVIDBABA"
-    SUPPORT_CHAT = "crazyworldchatting"  # Your own group for support, do not add the @
+    OWNER_ID = 5191956205  # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_USERNAME = "BANNA_XD"
+    SUPPORT_CHAT = "love_birds_group"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
-        -1001789156528
+        -1001511342850
     )  # Prints any new group the bot is added to, prints just the name and ID.
     EVENT_LOGS = (
-        -1001789156528
+        -1001511342850
     )  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
 
     # RECOMMENDED
@@ -35,7 +35,7 @@ class Config(object):
     INFOPIC = True
     URL = None
     SPAMWATCH_API = ""  # go to support.spamwat.ch to get key
-    SPAMWATCH_SUPPORT_CHAT = "@crazyworldchatting"
+    SPAMWATCH_SUPPORT_CHAT = "@love_birds_group"
 
     # OPTIONAL
     ##List of id's -  (not usernames) for users which have sudo access to the bot.
