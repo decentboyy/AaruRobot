9e695770f125916fa3b7901bf5cf48457afdc946
BANNA_XD <105870422+BANNA-XD143@users.noreply.github.com>
2022-07-07 06:23:26 +0530

    Update connection.py

--- AaruRobot/modules/connection.py
+++ AaruRobot/modules/connection.py
@@ -8,7 +8,7 @@ from telegram.ext import CommandHandler, CallbackQueryHandler, run_async
 import AaruRobot.modules.sql.connection_sql as sql
 from AaruRobot import dispatcher, DRAGONS, DEV_USERS
 from AaruRobot.modules.helper_funcs import chat_status
-from AaryRobot.modules.helper_funcs.alternate import send_message, typing_action
+from AaruRobot.modules.helper_funcs.alternate import send_message, typing_action
 
 user_admin = chat_status.user_admin
 
