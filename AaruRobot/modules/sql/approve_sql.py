94eb7045abc7ae2e21c33aaad7119b467b3ac52f
BANNA_XD <105870422+BANNA-XD143@users.noreply.github.com>
2022-07-07 10:06:27 +0530

    Update approve_sql.py

--- AaruRobot/modules/sql/approve_sql.py
+++ AaruRobot/modules/sql/approve_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String, UnicodeText, BigInteger, func, distinct
 
-from AyraRobot.modules.sql import BASE, SESSION
+from AaruRobot.modules.sql import BASE, SESSION
 
 
 class Approvals(BASE):
