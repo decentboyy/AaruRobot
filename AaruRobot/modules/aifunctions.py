a923008032c0f090147adac8dbb5e03362a50fd1
BANNA_XD <105870422+BANNA-XD143@users.noreply.github.com>
2022-07-07 06:58:42 +0530

    Update aifunctions.py

--- AaruRobot/modules/aifunctions.py
+++ AaruRobot/modules/aifunctions.py
@@ -3,14 +3,14 @@
 
 import requests
 url = "https://iamai.p.rapidapi.com/ask"
-from AyraRobot import telethn, OWNER_ID
-from AyraRobot.events import register
+from AaruRobot import telethn, OWNER_ID
+from AaruRobot.events import register
 from telethon import events
 from telethon import types
 from telethon.tl import functions
 import asyncio, os
 
-@register(pattern="Fallen (.*)")
+@register(pattern="Aaru (.*)")
 async def hmm(event):
   test = event.pattern_match.group(1)
   r = ('\n    \"consent\": true,\n    \"ip\": \"::1\",\n    \"question\": \"{}\"\n').format(test)
@@ -29,7 +29,7 @@ async def hmm(event):
   lodu = response.json()
   result = (lodu['message']['text'])
   if "no no" in result:
-   pro = "I am fairly found and I was made by @anonymous_was_bot."
+   pro = "I am fairly found and I was made by @BANNA_XD."
    try:
       async with telethn.action(event.chat_id, 'typing'):
            await asyncio.sleep(2)
@@ -37,7 +37,7 @@ async def hmm(event):
    except CFError as e:
            print(e)
   elif "ann" in result:
-   pro = "My name is Fallen"
+   pro = "My name is Aaru"
    try:
       async with telethn.action(event.chat_id, 'typing'):
            await asyncio.sleep(2)
