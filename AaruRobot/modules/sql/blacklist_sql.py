35f37a9ac57cdaefb0f657f057860003fdcaece5
BANNA_XD <105870422+BANNA-XD143@users.noreply.github.com>
2022-07-07 10:05:48 +0530

    Update blacklist_sql.py

--- AaruRobot/modules/sql/blacklist_sql.py
+++ AaruRobot/modules/sql/blacklist_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import func, distinct, Column, String, UnicodeText, BigInteger
 
-from AyraRobot.modules.sql import SESSION, BASE
+from AaruRobot.modules.sql import SESSION, BASE
 
 
 class BlackListFilters(BASE):
