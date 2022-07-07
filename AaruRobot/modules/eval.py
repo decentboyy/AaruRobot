8f0fa08aaa81e4b85274b3c8ca6bba209b969189
BANNA_XD <105870422+BANNA-XD143@users.noreply.github.com>
2022-07-07 05:54:37 +0530

    Update eval.py

--- AaruRobot/modules/eval.py
+++ AaruRobot/modules/eval.py
@@ -6,8 +6,8 @@ import textwrap
 import traceback
 from contextlib import redirect_stdout
 
-from AyraRobot import LOGGER, dispatcher
-from AyraRobot.modules.helper_funcs.chat_status import dev_plus
+from AaruRobot import LOGGER, dispatcher
+from AaruRobot.modules.helper_funcs.chat_status import dev_plus
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, CommandHandler, run_async
 
@@ -76,7 +76,7 @@ def do(func, bot, update):
 
     os.chdir(os.getcwd())
     with open(
-        os.path.join(os.getcwd(), "FallenRobot/modules/helper_funcs/temp.txt"), "w"
+        os.path.join(os.getcwd(), "AaruRobot/modules/helper_funcs/temp.txt"), "w"
     ) as temp:
         temp.write(body)
 
