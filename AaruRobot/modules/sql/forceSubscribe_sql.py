a9541136aa5217c4445b2a0440579b72f82bd3b5
BANNA_XD <105870422+BANNA-XD143@users.noreply.github.com>
2022-07-07 09:58:41 +0530

    Update forceSubscribe_sql.py

--- AaruRobot/modules/sql/forceSubscribe_sql.py
+++ AaruRobot/modules/sql/forceSubscribe_sql.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, String, Numeric, Boolean
-from AyraRobot.modules.sql import BASE, SESSION
+from AaruRobot.modules.sql import BASE, SESSION
 
 
 class forceSubscribe(BASE):
