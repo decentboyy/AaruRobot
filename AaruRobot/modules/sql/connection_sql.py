77498461cf73451e37a3eb30dd6d2200a014a9a9
BANNA_XD <105870422+BANNA-XD143@users.noreply.github.com>
2022-07-07 10:02:41 +0530

    Update connection_sql.py

--- AaruRobot/modules/sql/connection_sql.py
+++ AaruRobot/modules/sql/connection_sql.py
@@ -4,7 +4,7 @@ from typing import Union
 
 from sqlalchemy import Column, String, Boolean, UnicodeText, BigInteger
 
-from AyraRobot.modules.sql import SESSION, BASE
+from AaruRobot.modules.sql import SESSION, BASE
 
 
 class ChatAccessConnectionSettings(BASE):
