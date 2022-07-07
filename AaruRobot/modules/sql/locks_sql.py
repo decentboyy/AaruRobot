589382552d5879da8fc21c037e41cc516d7ea7e0
BANNA_XD <105870422+BANNA-XD143@users.noreply.github.com>
2022-07-07 09:57:21 +0530

    Update locks_sql.py

--- AaruRobot/modules/sql/locks_sql.py
+++ AaruRobot/modules/sql/locks_sql.py
@@ -3,7 +3,7 @@ import threading
 
 from sqlalchemy import Column, String, Boolean
 
-from AyraRobot.modules.sql import SESSION, BASE
+from AaruRobot.modules.sql import SESSION, BASE
 
 
 class Permissions(BASE):
