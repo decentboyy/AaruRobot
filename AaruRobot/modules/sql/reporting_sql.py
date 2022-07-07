14813ca13ca7c43cd339b513a7d3f7c4b32d2711
BANNA_XD <105870422+BANNA-XD143@users.noreply.github.com>
2022-07-07 09:53:57 +0530

    Update reporting_sql.py

--- AaruRobot/modules/sql/reporting_sql.py
+++ AaruRobot/modules/sql/reporting_sql.py
@@ -1,7 +1,7 @@
 import threading
 from typing import Union
 
-from AyraRobot.modules.sql import BASE, SESSION
+from AaruRobot.modules.sql import BASE, SESSION
 from sqlalchemy import Boolean, Column, BigInteger, String
 
 
