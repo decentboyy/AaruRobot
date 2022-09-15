b978d0423be69627300a3cfabc0ebdbe6ad1eb94
Mr-CODERxD <105870422+CODER-XD143@users.noreply.github.com>
2022-09-15 18:05:56 +0530

    Add files via upload

--- AaruRobot/modules/alive.py
+++ AaruRobot/modules/alive.py
@@ -1,19 +1,19 @@
+import os
+import re
 import random
-
-from pyrogram import __version__ as pyrover
+from platform import python_version as kontol
+from telethon import events, Button
 from telegram import __version__ as telever
-from telethon import Button
 from telethon import __version__ as tlhver
-
-from AaruRobot import OWNER_USERNAME, SUPPORT_CHAT, dispatcher
-from AaruRobot import telethn as tbot
+from pyrogram import __version__ as pyrover
 from AaruRobot.events import register
+from AaruRobot import telethn as tbot
+
 
 PHOTO = [
     "https://telegra.ph/file/99911b42bef56a2545ba4.jpg",
     "https://telegra.ph/file/38aacaa806d942f1fa2a9.jpg",
 ]
-
 
 @register(pattern=("/alive"))
 async def awake(event):
@@ -22,14 +22,8 @@ async def awake(event):
   TEXT += f"» **ʟɪʙʀᴀʀʏ ᴠᴇʀsɪᴏɴ :** `{telever}` \n\n"
   TEXT += f"» **ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{tlhver}` \n\n"
   TEXT += f"» **ᴘʏʀᴏɢʀᴀᴍ ᴠᴇʀsɪᴏɴ :** `{pyrover}` \n━━━━━━━━━━━━━━━━━\n\n"
-    BUTTON = [
-        [
-            Button.url("ʜᴇʟᴘ​", f"https://t.me/AARU_X_ROBOT?start=help"),
-            Button.url("sᴜᴘᴘᴏʀᴛ​", f"https://t.me/INDBRANDCHAT"),
-        ]
-    ]
-    ran = random.choice(PHOTO)
-    await tbot.send_file(event.chat_id, ran, caption=TEXT, buttons=BUTTON)
+  BUTTON = [[Button.url("ʜᴇʟᴘ​", "https://t.me/MISS_AARU_ROBOTt?start=help"), Button.url("sᴜᴘᴘᴏʀᴛ​", "https://t.me/INDBRANDCHAT")]]
+  ran = random.choice(PHOTO)
+  await tbot.send_file(event.chat_id, ran, caption=TEXT,  buttons=BUTTON)
 
-
-__mod_name__ = "Aʟɪᴠᴇ"
+## Alive mod
