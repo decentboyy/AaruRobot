c19deed72f3bdaf02a8b92e809ee7562b67363bf
Mr-CODERxD <105870422+CODER-XD143@users.noreply.github.com>
2023-01-14 09:15:09 +0530

    Add files via upload

--- AaruRobot/sample_config.py
+++ AaruRobot/sample_config.py
@@ -18,13 +18,13 @@ class Config(object):
     API_HASH = "awoo"
     TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
     OWNER_ID = 5352398272  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "XD_CODER"
-    SUPPORT_CHAT = "IND_BRAND_CHAT"  # Your own group for support, do not add the @
+    OWNER_USERNAME = "its_Coder_xD"
+    SUPPORT_CHAT = "LOVE_X_BIRDS"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
-        -1001774417977
+        -1001748978004
     )  # Prints any new group the bot is added to, prints just the name and ID.
     EVENT_LOGS = (
-        -1001774417977
+        -1001748978004
     )  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
 
     # RECOMMENDED
